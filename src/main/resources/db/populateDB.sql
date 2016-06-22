e5ac47bbbe05c8cd851da814f860361e63659d18
lessonsjul <julieee8909@gmail.com>
2016-06-23 02:37:02 +0300

    HW03 add db script for meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,7 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -11,3 +13,12 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (dateTime, description, calories, user_id)
+VALUES ('2015-05-30T10:00','Завтрак', 145, 100000);
+
+INSERT INTO meals (dateTime, description, calories, user_id)
+VALUES ('2015-05-30T13:00','Обед', 500, 100000);
+
+INSERT INTO meals (dateTime, description, calories, user_id)
+VALUES ('2015-05-30T20:00','Ужин', 700, 100000);
